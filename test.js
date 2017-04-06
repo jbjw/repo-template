cad1fe03dcff1e9604d735cba1a0225fcc830270
Jaxon Welsh <jaxonwelsh+github@gmail.com>
2017-04-05 17:47:42 -0700

    "sync"

--- test.js
+++ test.js
@@ -1,7 +1,13 @@
-const $scope = {}
-$scope.panes = {}
-$scope.panes.installs = {}
-$scope.panes.installs.save = function () {
-	let messages = [1, 2, 3]
+
+const width = 4
+const depth = 3
+
+// var myArr = new Array(width).map(() => new Array(depth).fill('?'))
+const myArr = new Array(width).fill(null).map(() => new Array(depth).fill('?'))
+console.log(myArr)
+myArr[0][0] = 'test'
+console.log(myArr)
+
+function render() {
 
 }
